d5e406caa3b0e200600402a215d7431a244e7a03
danicas <danicastroa@gmail.com>
2020-03-18 11:14:33 +0100

    Shader00

---
+++ Assets/Script/GlobalVariable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GlobalVariable : MonoBehaviour
+{
+    static readonly string TIMEVARIABLE = "_Time_Period";
+
+    public Slider slider;
+
+    public void NewValue()
+    {
+        Debug.Log(slider.value);
+        Shader.SetGlobalFloat(TIMEVARIABLE, slider.value);
+    }
+}
